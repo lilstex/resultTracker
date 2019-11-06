f5bf1fa42cec051ea32cf731d65f58ea2b0b3983
rexsteroxy <rexsteroxy22@gmail.com>
2019-11-06 15:17:13 +0100

    worked on the  app routing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ResultTracker' });
 });
 
+router.get('/app', function(req, res, next) {
+  res.render('app');
+});
+
 module.exports = router;
