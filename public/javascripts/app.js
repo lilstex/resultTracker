519bae08a08cacc0d6d02370d0e21427ad3ab114
rexsteroxy <rexsteroxy22@gmail.com>
2019-11-27 14:53:15 +0100

    worked on the frontend issues

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -206,7 +206,7 @@
                 }
                 else {
                     display.style.margin = '5px';
-                    display.style.border = '2px solide black';
+                    display.style.border = '2px solid black';
                     display.style.padding = '24px';
                     display.style.borderRadius = '12px';
                     display.style.backgroundColor = 'rgb(6, 192, 167)';
@@ -227,27 +227,57 @@
                 resultDisplay.innerHTML = `Your GP Calculation IS ${mgp}`;
 
                 if (mgp >= 4.5) {
+                    resultDisplay.style.textAlign = 'center';
+                     resultDisplay.style.margin = '5px';
+                     resultDisplay.style.border = '2px solid black';
+                     resultDisplay.style.padding = '24px';
+                     resultDisplay.style.borderRadius = '12px';
                     resultDisplay.style.color = '#ffffff';
                     resultDisplay.style.backgroundColor = 'rgb(6, 192, 167)';
                     savebtn.style.display = 'initial';
                 } else if (mgp >= 3.5 && mgp < 4.5) {
+                    resultDisplay.style.textAlign = 'center';
+                     resultDisplay.style.margin = '5px';
+                     resultDisplay.style.border = '2px solid black';
+                     resultDisplay.style.padding = '24px';
+                     resultDisplay.style.borderRadius = '12px';
                     resultDisplay.style.color = '#ffffff';
                     resultDisplay.style.backgroundColor = '#001200';
                     savebtn.style.display = 'initial';
                 }
                 else if (mgp >= 3 && mgp < 3.5) {
+                    resultDisplay.style.textAlign = 'center';
+                     resultDisplay.style.margin = '5px';
+                     resultDisplay.style.border = '2px solid black';
+                     resultDisplay.style.padding = '24px';
+                     resultDisplay.style.borderRadius = '12px';
                     resultDisplay.style.color = '#ffffff';
                     resultDisplay.style.backgroundColor = '#f19000';
                     savebtn.style.display = 'initial';
                 } else if (mgp >= 2 && mgp < 3) {
+                    resultDisplay.style.textAlign = 'center';
+                     resultDisplay.style.margin = '5px';
+                     resultDisplay.style.border = '2px solid black';
+                     resultDisplay.style.padding = '24px';
+                     resultDisplay.style.borderRadius = '12px';
                     resultDisplay.style.color = '#ffffff';
                     resultDisplay.style.backgroundColor = '#e15000';
                     savebtn.style.display = 'initial';
                 } else if (mgp >= 0 && mgp < 2) {
+                    resultDisplay.style.textAlign = 'center';
+                    resultDisplay.style.margin = '5px';
+                    resultDisplay.style.border = '2px solid black';
+                    resultDisplay.style.padding = '24px';
+                    resultDisplay.style.borderRadius = '12px';
                     resultDisplay.style.color = '#ffffff';
                     resultDisplay.style.backgroundColor = '#ff0000';
                     savebtn.style.display = 'initial';
                 } else {
+                    resultDisplay.style.textAlign = 'center';
+                     resultDisplay.style.margin = '5px';
+                     resultDisplay.style.border = '2px solid black';
+                     resultDisplay.style.padding = '24px';
+                     resultDisplay.style.borderRadius = '12px';
                     resultDisplay.style.color = '#ffffff';
                     resultDisplay.style.backgroundColor = '#ff0000';
                     resultDisplay.innerHTML = `Please enter course unit`;
